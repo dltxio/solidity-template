8565e9afda0f107ea29e34481fd2b768a6e8d2ab
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-09-03 09:00:44 +1000

    import coverage dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-contract-sizer";
 import "./scripts/tasks"
+import "solidity-coverage"
 import {ethers} from "ethers";
 
 export default {
