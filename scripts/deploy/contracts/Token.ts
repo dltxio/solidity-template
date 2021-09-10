21e59a662a41a7abdea3decee0edb8573b9a316f
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-09-10 16:06:53 +1000

    prepare deployment routine for upgradeable contracts

--- scripts/deploy/contracts/Token.ts
+++ scripts/deploy/contracts/Token.ts
@@ -1,14 +1,18 @@
 import { deployContract } from "../utils";
 
+export const args = ["token"];
+
+export const constructorArguments = () => [
+  process.env.CONSTRUCTOR_TOKEN_NAME,
+  process.env.CONSTRUCTOR_TOKEN_SYMBOL,
+  process.env.CONSTRUCTOR_TOKEN_DECIMALS,
+];
+
 export const deploy = async (setAddresses) => {
   console.log("deploying Token");
   const token = await deployContract(
     "Token",
-    [
-        process.env.CONSTRUCTOR_TOKEN_NAME,
-        process.env.CONSTRUCTOR_TOKEN_SYMBOL,
-        process.env.CONSTRUCTOR_TOKEN_DECIMALS,
-    ],
+    constructorArguments(),
     1
   );
   console.log(`deployed Token to address ${token.address}`);
