f4a4e9e402bb54f9418b101f4813e9636daa6d1d
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-10-13 13:00:18 +1000

    fix token mint task

--- scripts/tasks/token.ts
+++ scripts/tasks/token.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config";
 import contracts from "../../contracts.json";
+import {getLedgerSigner} from "../utils";
 
 task("read-balance")
   .addParam("address")
@@ -16,17 +17,23 @@ task("read-balance")
 task("mint")
   .addParam("address")
   .addParam("amount")
+  .addOptionalParam("ledgersigner")
   .setAction(async (args, hre) => {
-    const [deployer] = await hre.ethers.getSigners();
+    let signer;
+    if (!args.ledgersigner) {
+      signer = (await hre.ethers.getSigners())[0];
+    } else {
+      signer = getLedgerSigner(args.ledgersigner, hre.ethers.provider);
+    }
     const contractAddress = contracts[hre.network.name].token;
     console.log(`network is ${hre.network.name}`);
     console.log(`token address is ${contractAddress}`)
     const token = (await hre.ethers.getContractFactory("Token"))
-      .attach(contractAddress);
+      .attach(contractAddress)
+      .connect(signer);
     const decimals = await token.decimals();
-    const ten = hre.ethers.BigNumber.from("10");
     // Mint with the correct decimals.
-    const mintAmount = ten.pow(decimals).mul(args.amount);
+    const mintAmount = hre.ethers.utils.parseUnits(args.amount, decimals);
     const receipt = await token.mint(args.address, mintAmount);
     console.log("waiting for confirmation...");
     await receipt.wait(1);
