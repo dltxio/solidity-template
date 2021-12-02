a26e7756431ad63ac83a7f710fa52c6200e58547
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-12-02 14:09:55 +1000

    Feat: remove safe math

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.0;
 pragma abicoder v2;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is IERC20, ERC20, Ownable {
-    using SafeMath for uint256;
-
     uint8 private immutable _decimals;
 
     constructor(
