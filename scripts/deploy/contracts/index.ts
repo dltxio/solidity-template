abb970fcbb82993b075058c223d25a9356b7ca4b
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2022-09-23 16:27:22 +1000

    feat: modularise deployment types

--- scripts/deploy/contracts/index.ts
+++ scripts/deploy/contracts/index.ts
@@ -1,18 +1,35 @@
-import { ethers as tsEthers } from "ethers";
+import { Signer, Contract } from "ethers";
 import * as Token from "./Token";
 import * as TokenUpgradeable from "./TokenUpgradeable";
 import * as NFT from "./NFT";
 import * as NFTa from "./NFTa";
 
+/**
+ *  A deployment is an input object consisting of the deployment name
+ *  and constructor arguments.
+ */
+export type Deployment = {
+  name: string;
+  constructorArguments: string[];
+};
+
+export type DeploymentFunction = (
+  name: string,
+  deployer: Signer,
+  setAddresses: Function,
+  addresses?: any
+) => Promise<Contract>;
+
+export type UpgradeFunction = (
+  name: string,
+  deployer: Signer,
+  addresses?: any
+) => void;
+
 export interface DeploymentModule {
-  contractNames: (...params: any) => string[];
-  constructorArguments: (addresses?: any) => any[];
-  deploy: (
-    deployer: tsEthers.Signer,
-    setAddresses: Function,
-    addresses?: any
-  ) => Promise<tsEthers.Contract>;
-  upgrade?: (deployer: tsEthers.Signer, addresses?: any) => void;
+  deploy: DeploymentFunction;
+  deployments: () => Deployment[];
+  upgrade?: UpgradeFunction;
 }
 
 const modules: DeploymentModule[] = [Token, TokenUpgradeable, NFT, NFTa];
