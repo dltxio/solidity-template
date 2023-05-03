e40f66f10988e33e4ef719531620dd5a52c0475d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-05-03 10:46:30 +1000

    chore: rebuild

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,69 +1,8 @@
-import { config as dotenvConfig } from "dotenv";
-dotenvConfig();
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "@openzeppelin/hardhat-upgrades";
-import "@nomiclabs/hardhat-etherscan";
-import "hardhat-contract-sizer";
-import "./scripts/tasks";
-import "solidity-coverage";
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-const defaultKey =
-  "0000000000000000000000000000000000000000000000000000000000000001";
-const defaultRpcUrl = "https://localhost:8545";
-
-export default {
-  gasReporter: {
-    enabled: true,
-    currency: "ETH",
-    gasPrice: "auto",
-    showInChart: true
-  },
-  paths: {
-    sources: "./contracts",
-    cache: "./cache",
-    artifacts: "./artifacts",
-    tests: "./tests"
-  },
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      allowUnlimitedContractSize: false
-    },
-    sepolia: {
-      url: process.env.SEPOLIA_URL || defaultRpcUrl,
-      accounts: [process.env.PRIVATE_KEY || defaultKey]
-    },
-    mainnet: {
-      url: process.env.MAINNET_URL || defaultRpcUrl,
-      accounts: [process.env.PRIVATE_KEY || defaultKey]
-    }
-  },
-  etherscan: {
-    // Obtain etherscan API key at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_KEY
-  },
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.4",
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200
-          }
-        }
-      }
-    ]
-  },
-  typechain: {
-    outDir: "build/typechain",
-    target: "ethers-v5",
-    alwaysGenerateOverloads: false,
-    externalArtifacts: ["externalArtifacts/*.json"]
-  }
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
 };
+
+export default config;
