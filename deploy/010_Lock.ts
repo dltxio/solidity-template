3ed527da438360ec687ba0e5b68ce9fa7f7ca6f7
Barry Earsman <barry@earsman.com>
2023-05-09 15:25:54 +1000

    chore: back to using spaces to GH merges look better

--- deploy/010_Lock.ts
+++ deploy/010_Lock.ts
@@ -3,22 +3,22 @@ import "hardhat-deploy";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	const { deployments, getNamedAccounts } = hre;
-	const { deploy } = deployments;
-	const namedAccounts = await getNamedAccounts();
-	const { deployer } = namedAccounts;
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const namedAccounts = await getNamedAccounts();
+  const { deployer } = namedAccounts;
 
-	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-	const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-	await deploy("Lock", {
-		contract: "Lock",
-		from: deployer,
-		args: [unlockTime],
-		log: true,
-		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
-		skipIfAlreadyDeployed: true
-	});
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  await deploy("Lock", {
+    contract: "Lock",
+    from: deployer,
+    args: [unlockTime],
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
+    skipIfAlreadyDeployed: true
+  });
 };
 export default func;
 func.tags = ["example"];
